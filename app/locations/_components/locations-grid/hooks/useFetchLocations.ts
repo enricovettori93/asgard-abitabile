09b7814f52bfa999aaa7e5ec073c0850f437e6e4
Enrico Vettori <enrico.vettori93@gmail.com>
2024-05-20 15:12:05 +0200

    refactor(locations pagination): changed from a sort of "infinite scroll" method to a classic pagination

--- app/locations/_components/locations-grid/hooks/useFetchLocations.ts
+++ app/locations/_components/locations-grid/hooks/useFetchLocations.ts
@@ -1,40 +1,21 @@
-import {useState} from "react";
 import {useRouter, useSearchParams} from "next/navigation";
-import betterFetch from "@/utils/fetch";
-import {LocationWithPictures} from "@/types/location";
 
-interface props {
-    data: LocationWithPictures[]
-    currentPage: number
-}
-
-const useFetchLocations = ({data, currentPage}: props) => {
-    const [loading, setLoading] = useState(false);
-    const [locations, setLocations] = useState(data);
+const useFetchLocations = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
 
-    const handleFetch = async () => {
-        setLoading(true);
+    const pageChange = async (page: number) => {
         const queryParams = new URLSearchParams(searchParams.toString());
-        const currentPage = Number(queryParams.get("page")) ?? 1;
-        // fixme: seems to reload the component and fetch multiple time
-        queryParams.set("page", `${currentPage + 1}`);
-        router.replace(`?${queryParams.toString()}`, {scroll: false});
-        const {data = []} = await betterFetch<LocationWithPictures[]>(`locations?${queryParams}`);
-
-        setLocations(prev => [...prev, ...data]);
-        setLoading(false);
+        queryParams.set("page", `${page}`);
+        router.push(`?${queryParams.toString()}`);
     }
 
-    const handleLoadMore = async () => {
-        await handleFetch();
+    const handlePageChange = async (page: number) => {
+        await pageChange(page);
     }
 
     return {
-        locations,
-        loading,
-        handleLoadMore
+        handlePageChange
     }
 }
 
--- app/locations/_components/locations-grid/index.tsx
+++ app/locations/_components/locations-grid/index.tsx
@@ -3,6 +3,10 @@
 import {LocationWithPictures} from "@/types/location";
 import useFetchLocations from "@/app/locations/_components/locations-grid/hooks/useFetchLocations";
 import CardLocation from "@/app/locations/_components/location-card";
+import ReactPaginate from "react-paginate";
+import {PAGE_SIZE, ROUTES} from "@/utils/constants";
+import {useSearchParams} from "next/navigation";
+import classNames from "classnames";
 
 interface props {
     data: LocationWithPictures[]
@@ -10,21 +14,46 @@ interface props {
     currentPage: number
 }
 
+// from ReactPaginate src
+type PageChangeEvent = {
+    selected: number
+}
+
 const LocationsGrid = ({data, totalElements, currentPage}: props) => {
-    const {locations, loading, handleLoadMore} = useFetchLocations({data, currentPage})
+    const params = useSearchParams();
+    const {handlePageChange} = useFetchLocations();
+    const pageCount = Math.floor(totalElements / PAGE_SIZE);
+
+    const pageChange = async ({selected}: PageChangeEvent) => {
+        await handlePageChange(selected + 1);
+    }
+
+    const buildHref = (pageNumber: number) => {
+        const queryString = params.toString();
+        return `${ROUTES.LOCATIONS}?${queryString.replace(/(page=)\d+/, `$1${pageNumber}`)}`;
+    }
+
+    const paginationClasses = classNames({
+        "pagination my-5": true,
+        "pagination--one-page": pageCount <= 1
+    });
 
     return (
         <section>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-                {locations.map(location => <CardLocation key={location.id} location={location}/>)}
+                {data.map(location => <CardLocation key={location.id} location={location}/>)}
             </div>
-            {
-                locations.length !== totalElements && (
-                    <div className="flex justify-center my-10">
-                        <button disabled={loading} className="button--primary" onClick={handleLoadMore}>Load more</button>
-                    </div>
-                )
-            }
+            <div className="flex justify-end">
+                <ReactPaginate
+                    className={paginationClasses}
+                    pageCount={pageCount}
+                    onPageChange={pageChange}
+                    hrefBuilder={buildHref}
+                    nextLabel=">"
+                    previousLabel="<"
+                    activeClassName="page--active"
+                />
+            </div>
         </section>
     )
 }
